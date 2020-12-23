ded2fe14db298b6e25468629fcd51e7700d89610
Ilija <komperdel@gmail.com>
2020-12-23 12:59:19 +0200

    all

--- src/js/script.js
+++ src/js/script.js
@@ -1,23 +1,32 @@
-/* $(document).ready(function(){
-    $('.carousel__inner').slick({
-        speed: 1200,
-        adaptiveHeight: false,
-        prevArrow:'<button type="button" class="slick-prev"><img src="../img/icons/slider/chevron-left-solid.png"></button>' ,
-        nextArrow:'<button type="button" class="slick-next"><img src="../img/icons/slider/chevron-right-solid.png"></button>' ,
-        responsive: [
+$(document).ready(function(){
+  $('.carousel__inner').slick({
+      speed: 1200,
+      adaptiveHeight: false,
+      prevArrow: '<button type="button" class="slick-prev"><img src="../img/icons/slider/chevron-left-solid.svg"></button>',
+      nextArrow: '<button type="button" class="slick-next"><img src="../img/icons/slider/chevron-right-solid.svg"></button>',
+      responsive: [
           {
-            breakpoint: 992,
-            settings: {
-              dots: true,
-              arrows: false,
-            }
+              breakpoint: 992,
+              settings: {
+                  dots: true,
+                  arrows: false,
+              }
           }
-        ]
-      });
-}); */
+      ]
+  });
+
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+      .addClass('.catalog__tab_active').siblings().removeClass('.catalog__tab_active')
+      .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+  });
+});
 
 
-const slider = tns({
+
+
+
+/* const slider = tns({
   container: '.carousel__inner',
   items: 1,
   slideBy: 'page',
@@ -34,3 +43,4 @@ document.querySelector('.prev').addEventListener('click', function () {
 document.querySelector('.next').addEventListener('click', function () {
   slider.goTo('next');
 });
+ */
